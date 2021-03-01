4db2d4d95dd86b3552a7b9e7bf8e2bb711e70605
Muneeb Ur Rehman <m.muneeb36@gmail.com>
2021-03-01 11:54:04 +0500

    Add files via upload
    Carrosal Style App

---
+++ app/src/main/java/com/muneeb_dev/mycarosalapplication/MainActivity.java
@@ -0,0 +1,90 @@
+package com.muneeb_dev.mycarosalapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ViewPager viewpagerTop, viewPagerBackground;
+    public static final int ADAPTER_TYPE_TOP = 1;
+    public static final int ADAPTER_TYPE_BOTTOM = 2;
+
+    private int[] listItems = {R.drawable.img1, R.drawable.img2, R.drawable.img3, R.drawable.img4,
+            R.drawable.img5, R.drawable.img6, R.drawable.img7, R.drawable.img8, R.drawable.img9, R.drawable.img10};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+        setupViewPager();
+    }
+
+    /**
+     * Initialize all required variables
+     */
+    private void init() {
+        viewpagerTop = (ViewPager) findViewById(R.id.viewpagerTop);
+        viewPagerBackground = (ViewPager) findViewById(R.id.viewPagerbackground);
+
+        viewpagerTop.setClipChildren(false);
+        viewpagerTop.setPageMargin(getResources().getDimensionPixelOffset(R.dimen.pager_margin));
+        viewpagerTop.setOffscreenPageLimit(3);
+        viewpagerTop.setPageTransformer(false, new CarouselEffectTransformer(this)); // Set transformer
+    }
+
+
+    /**
+     * Setup viewpager and it's events
+     */
+    private void setupViewPager() {
+        // Set Top ViewPager Adapter
+        MyPagerAdapter adapter = new MyPagerAdapter(this, listItems, ADAPTER_TYPE_TOP);
+        viewpagerTop.setAdapter(adapter);
+
+        // Set Background ViewPager Adapter
+        MyPagerAdapter adapterBackground = new MyPagerAdapter(this, listItems, ADAPTER_TYPE_BOTTOM);
+        viewPagerBackground.setAdapter(adapterBackground);
+
+
+        viewpagerTop.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+
+            private int index = 0;
+
+            @Override
+            public void onPageSelected(int position) {
+                index = position;
+
+            }
+
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                int width = viewPagerBackground.getWidth();
+                viewPagerBackground.scrollTo((int) (width * position + width * positionOffset), 0);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+                if (state == ViewPager.SCROLL_STATE_IDLE) {
+                    viewPagerBackground.setCurrentItem(index);
+                }
+
+            }
+        });
+    }
+
+    public void clickEvent(View view) {
+        switch (view.getId()) {
+            case R.id.linMain:
+                if (view.getTag() != null) {
+                    int poisition = Integer.parseInt(view.getTag().toString());
+                    Toast.makeText(getApplicationContext(), "Poistion: " + poisition, Toast.LENGTH_LONG).show();
+                }
+                break;
+        }
+    }
+}
